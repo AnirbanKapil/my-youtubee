2904aa23d208bf4aca86f28f53336f077967aa55
Anirban Kapil <anirbankapil@gmail.com>
2024-04-05 17:16:15 +0530

    initial setup

---
+++ src/components/ButtonList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ButtonList () {
+    return (
+        <div>
+            <h1>Button List</h1>
+        </div>
+    )
+};
+
+
+export default ButtonList;
---
+++ src/components/VideoContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function VideoContainer () {
+    return (
+        <div>
+            <h1>VideoContainer</h1>
+        </div>
+    )
+};
+
+export default VideoContainer;
