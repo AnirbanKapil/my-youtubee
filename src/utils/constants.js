cb0a9fb5f3db5ce3e9dd947b22905b03ae6c8eea
Anirban Kapil <anirbankapil@gmail.com>
2024-06-16 23:53:53 +0530

    youtube api handling

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+
+
+
+
+const API_CALL = "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&key=AIzaSyBD0W2VpyERhPETRvvD9R6-aNF8LveCdz8"
+
+
+
+export default API_CALL;
+
+
+
+
+
+
+
+
+
