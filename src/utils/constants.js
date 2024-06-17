c3a9b786a38a9ef0ea8fac1bc51d1e4412b23daa
Anirban Kapil <anirbankapil@gmail.com>
2024-06-17 16:29:51 +0530

    fetching youtube api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 
-const GOOGLE_API_KEY = "AIzaSyCEcXCbDie-HyNDvdTRhIr5zOXBY0BFlgA"
+const GOOGLE_API_KEY = "AIzaSyBlpOJ4UYQ2MTGReZ7VoMoOrSgioGYdoZc"
 
 
 export const API_CALL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY;
