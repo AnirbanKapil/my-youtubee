560247599ff02c94684abb148bf200f5ba31f8bf
Anirban Kapil <anirbankapil@gmail.com>
2024-06-21 23:57:22 +0530

    setting up comment section

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../utils/appSlice'
 import { useSearchParams } from 'react-router-dom'
+import Comments from './Comments'
 
 function WatchPage() {
   
@@ -14,11 +15,14 @@ function WatchPage() {
   },[])  
 
   return (
+    <div className='flex flex-col'>
     <div className='p-4 m-4'>
         <iframe width="960" height="515" src={"https://www.youtube.com/embed/"+searchParams.get("v")}
          title="YouTube video player" frameBorder="0" 
          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
          referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </div>
+       <Comments />
     </div>
   )
 }
